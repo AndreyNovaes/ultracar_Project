d2fab8be23d90a1978bd8ddd7f482af216241c70
AndreyNovaes <andreynovaespro@gmail.com>
2023-04-18 18:45:51 -0300

    feat: add main component, react starter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+// routing
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './router';
+// antd
+import { ConfigProvider, theme } from 'antd';
+import './theme.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <ConfigProvider theme={theme}>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </ConfigProvider>,
+  document.getElementById('root')
+);
